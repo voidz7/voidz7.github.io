f76c250bc5f52a967cf4d8274aa9a0c6909399bb
voidz <atharvask2005@gmail.com>
2021-08-21 02:47:16 +0530

    :candy:

---
+++ app.js
@@ -0,0 +1,48 @@
+const endpoint = `https://api.github.com/users/voidz7/repos`;
+
+const getRepos = (repos) => {
+
+    const newRepos =
+        repos
+            .sort(
+                (a, b) => Number(new Date(b.updated_at)) - Number(new Date(a.updated_at))
+            )
+            .slice(0, 2);
+
+    const [
+        {
+            name: firstRepoName,
+            html_url: firstRepoURL,
+            description: firstRepoDescription
+        },
+        {
+            name: secondRepoName,
+            html_url: secondRepoURL,
+            description: secondRepoDescription
+        }
+    ] = newRepos;
+
+   document.querySelector(".repos").innerHTML = 
+   `
+   
+   <a class = "repoLink" target="_blank" rel="noopener noreferrer" href="${firstRepoURL}">"${firstRepoName}"</a>
+   (${firstRepoDescription}) and 
+   <a class = "repoLink" target="_blank" rel="noopener noreferrer" href="${secondRepoURL}">"${secondRepoName}"</a>
+   (${secondRepoDescription})
+
+   `
+
+
+}
+
+const fetchData = () => {
+
+    fetch(endpoint)
+        .then(res => res.json())
+        .then(data => getRepos(data));
+
+}
+
+
+fetchData();
+
