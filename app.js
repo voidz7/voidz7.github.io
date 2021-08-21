d655d69f0ff52b80e45de17e6d02df86687e9469
voidz <atharvask2005@gmail.com>
2021-08-21 11:03:30 +0530

    added proper layout to recent commits

--- app.js
+++ app.js
@@ -1,14 +1,12 @@
 const endpoint = `https://api.github.com/users/voidz7/repos`;
 
 const getRepos = (repos) => {
-
     const newRepos =
         repos
             .sort(
                 (a, b) => Number(new Date(b.updated_at)) - Number(new Date(a.updated_at))
             )
             .slice(0, 2);
-
     const [
         {
             name: firstRepoName,
@@ -25,24 +23,17 @@ const getRepos = (repos) => {
    document.querySelector(".repos").innerHTML = 
    `
    
-   <a class = "repoLink" target="_blank" rel="noopener noreferrer" href="${firstRepoURL}">"${firstRepoName}"</a>
-   (${firstRepoDescription}) and 
-   <a class = "repoLink" target="_blank" rel="noopener noreferrer" href="${secondRepoURL}">"${secondRepoName}"</a>
+   -> <a class = "repoLink" target="_blank" rel="noopener noreferrer" href="${firstRepoURL}">"${firstRepoName}"</a>
+   (${firstRepoDescription}) <br>
+   -> <a class = "repoLink" target="_blank" rel="noopener noreferrer" href="${secondRepoURL}">"${secondRepoName}"</a>
    (${secondRepoDescription})
 
    `
-
-
 }
 
 const fetchData = () => {
-
     fetch(endpoint)
         .then(res => res.json())
         .then(data => getRepos(data));
-
 }
-
-
 fetchData();
-
